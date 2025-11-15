6aa1fcec3c1c7ce7c8b4d72861702989ac3ecf99
hyein89 <bihontai@gmail.com>
2025-11-15 14:06:53 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,17 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* PENTING: pasang font Material Symbols di sini, BUKAN di halaman */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0"
+          rel="stylesheet"
+        />
       </Head>
+
       <Component {...pageProps} />
     </>
   )
