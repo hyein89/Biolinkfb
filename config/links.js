cf07ec93b6433d962953ec23be613695dc03790e
hyein89 <bihontai@gmail.com>
2025-11-15 19:15:06 +0700

    Create links.js

---
+++ config/links.js
@@ -0,0 +1,12 @@
+// Semua link ada di sini, biar gampang edit
+const LINKS = {
+  OFFER: "https://contoh-offer.com/landing69",
+
+  WHATSAPP: "https://wa.me/6281234567890",
+
+  TELEGRAM: "https://t.me/usernameanda",
+
+  WEBSITE: "https://example.com"
+};
+
+export default LINKS;
