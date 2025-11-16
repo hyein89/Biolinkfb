3fb826919b3f7f0f0cc7d72aabbc9de8727d01b6
hyein89 <bihontai@gmail.com>
2025-11-16 11:14:35 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,21 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  async headers() {
-    return [
+const nextConfig = {
+  images: {
+    remotePatterns: [
       {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src 'self' https://s10.histats.com https://sstatic1.histats.com; " +
-              "script-src 'self' 'unsafe-inline' https://s10.histats.com; " +
-              "img-src 'self' https://sstatic1.histats.com data:; " +
-              "connect-src 'self' https://s10.histats.com; " +
-              "style-src 'self' 'unsafe-inline'; " +
-              "font-src 'self' https://fonts.gstatic.com;"
-          }
-        ]
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "i0.wp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "biolinkfb.vercel.app",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
+
+module.exports = nextConfig;
