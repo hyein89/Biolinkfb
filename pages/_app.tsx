fe0eb7171d5ff16068cb4d30cc3844de890fbbaf
hyein89 <bihontai@gmail.com>
2025-11-16 11:39:49 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Script from 'next/script'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -10,8 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/png" href="/2497746.png" />
         <meta name="theme-color" content="#acd84d" />
-        
-        {/* Fonts */}
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
@@ -20,39 +18,18 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       </Head>
 
-      {/* ================= HISTATS ==================== */}
-      <Script id="histats" strategy="afterInteractive">
-        {`
-          window._Hasync = window._Hasync || [];
-          window._Hasync.push(['Histats.start', '4828760', '4', '0', '0', '0']);
-          window._Hasync.push(['Histats.fasi', '1']);
-          window._Hasync.push(['Histats.track_hits', '']);
-
-          function loadHistats() {
-            var hs = document.createElement('script');
-            hs.type = 'text/javascript';
-            hs.async = true;
-            hs.src = 'https://s10.histats.com/js15_as.js';
-            document.body.appendChild(hs);
-          }
-
-          if (document.readyState === 'complete') {
-            loadHistats();
-          } else {
-            window.addEventListener('load', loadHistats);
-          }
-        `}
-      </Script>
-
-      <noscript>
-        <a href="/" target="_blank">
-          <img
-            src="https://sstatic1.histats.com/0.gif?4828760&101"
-            alt="histats"
-          />
-        </a>
-      </noscript>
-      {/* =============== END HISTATS ================= */}
+      {/* ================= HISTATS VIA IFRAME ==================== */}
+      <iframe
+        src="/histats.html"
+        style={{
+          width: 0,
+          height: 0,
+          border: 'none',
+          position: 'absolute',
+          visibility: 'hidden'
+        }}
+      ></iframe>
+      {/* ================= END HISTATS ==================== */}
 
       <Component {...pageProps} />
     </>
