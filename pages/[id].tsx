6c4ccc42e21abb3fb6c005e9b5643a68eda4163e
hyein89 <bihontai@gmail.com>
2025-11-16 11:23:03 +0700

    Update [id].tsx

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -4,7 +4,7 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
 import fs from "fs";
 import path from "path";
-
+import Script from "next/script";
 // ========================
 // TYPE
 // ========================
@@ -79,40 +79,26 @@ export default function LinkPage({ item, absoluteImage }: Props) {
         />
       </Head>
 
-{/* ================= HISTATS ==================== */}
+{/* HISTATS */}
       <Script id="histats" strategy="afterInteractive">
         {`
-          window._Hasync = window._Hasync || [];
-          window._Hasync.push(['Histats.start', '4828760', '4', '0', '0', '0']);
-          window._Hasync.push(['Histats.fasi', '1']);
-          window._Hasync.push(['Histats.track_hits', '']);
-
-          function loadHistats() {
-            var hs = document.createElement('script');
-            hs.type = 'text/javascript';
-            hs.async = true;
-            hs.src = 'https://s10.histats.com/js15_as.js';
-            document.body.appendChild(hs);
-          }
-
-          if (document.readyState === 'complete') {
-            loadHistats();
-          } else {
-            window.addEventListener('load', loadHistats);
-          }
+          var _Hasync = _Hasync || [];
+          _Hasync.push(['Histats.start', '1,4828760,4,0,0,0,00010000']);
+          _Hasync.push(['Histats.fasi', '1']);
+          _Hasync.push(['Histats.track_hits', '']);
+          (function() {
+            var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+            hs.src = ('//s10.histats.com/js15_as.js');
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+          })();
         `}
       </Script>
 
       <noscript>
         <a href="/" target="_blank">
-          <img
-            src="https://sstatic1.histats.com/0.gif?4828760&101"
-            alt="histats"
-          />
+          <img src="//sstatic1.histats.com/0.gif?4828760&101" alt="" />
         </a>
       </noscript>
-      {/* =============== END HISTATS ================= */}
-
       
       {/* BODY */}
       <div className="container">
