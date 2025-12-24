a69fbe918d95cbe2c849cad7cb1d2e604c3765be
hyein89 <bihontai@gmail.com>
2025-12-25 05:13:07 +0700

    Update links.js

--- config/links.js
+++ config/links.js
@@ -2,9 +2,9 @@
 const LINKS = {
   OFFER: "https://wedebeek.com/click?pid=3574&offer_id=19525&sub1=Kon",
 
-  WHATSAPP: "http://socialsmdom.com/SzJ8wsdKES",
+  WHATSAPP: "https://signingunwilling.com/ct17kekvb?key=3cdbaeb0a1e2067eb3683d5a6bf235e3",
 
-  TELEGRAM: "http://socialsmdom.com/SzJ8wsdKES",
+  TELEGRAM: "https://signingunwilling.com/ct17kekvb?key=3cdbaeb0a1e2067eb3683d5a6bf235e3",
 
   WEBSITE: "https://dothojmlsjuns.eu.org/?id=dafulat"
 };
